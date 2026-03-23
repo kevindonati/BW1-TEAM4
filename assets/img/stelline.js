74dc5f9e1ab92b0e07f06676abf2f527c5e92f35
Gianni <BussolettiGianni@gmail.com>
2026-03-23 21:59:40 +0100

    Star Rating Done

--- assets/img/stelline.js
+++ assets/img/stelline.js
@@ -1,7 +1,11 @@
-const changeStyle = (event) => {
-  const img = document.querySelectorAll("#stelline img")
+const changeStyle = (n) => {
+  const img = document.querySelectorAll("#stelline path")
 
-  for (let i = 0; i < event.length; i++) {
-    img[i].classlist.add("stella-scura")
+  for (let i = 0; i < n; i++) {
+    img[i].setAttribute("fill", "#202139")
+  }
+  for (let i = 9; i > n - 1; i--) {
+    img[i].setAttribute("fill", "#00FFFF")
   }
 }
+// }
