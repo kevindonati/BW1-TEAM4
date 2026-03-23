73e3c28760c3fd86cc434c2b042e2f0241b5c5e3
Niccolò Sciucco <sciucco.niccolo@gmail.com>
2026-03-23 15:23:37 +0100

    Update script.js

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,42 +1,50 @@
 // ANIMAZIONE DEL TIMER
 
-const tempoTimer = 10 // Durata del timer
+const tempoTimer = 10; // Durata del timer
 
-let tempoRimanente = tempoTimer
-let contoAllaRovescia
+let tempoRimanente = tempoTimer;
+let contoAllaRovescia;
 
-const numeriTimer = document.querySelector(".numeri-timer")
-const progressoTimer = document.querySelector(".progresso-timer")
-const lineaCountdown = 283 // numero per fare la diminuire la linea in maniera giusta
+const numeriTimer = document.querySelector(".numeri-timer");
+const progressoTimer = document.querySelector(".progresso-timer");
+const lineaCountdown = 283; // numero per fare la diminuire la linea in maniera giusta
 
 const updateCountdown = () => {
   // costante che cambia il numero e il colore della linea quando viene cambiata
-  numeriTimer.innerText = tempoRimanente
+  numeriTimer.innerText = tempoRimanente;
 
-  const updateLinea = lineaCountdown - (tempoRimanente / tempoTimer) * lineaCountdown
-  progressoTimer.style.strokeDashoffset = updateLinea
+  const updateLinea =
+    lineaCountdown - (tempoRimanente / tempoTimer) * lineaCountdown;
+  progressoTimer.style.strokeDashoffset = updateLinea;
   // Formula matematica che server per far diminure la linea in modo giusto. NON CHIEDERE mi fido di chi l'ha scritta
-}
+};
 
 const tempoFinito = () => {
-  alert("tempo scaduto!")
+  alert("tempo scaduto!");
   // qui andranno inseriti i comportamenti di quello che succederà una volta che il timer sarà scaduto
-} // alert che avverte che il timer è scaduto
+}; // alert che avverte che il timer è scaduto
 
 const inizioContoRovescia = () => {
-  tempoRimanente = tempoTimer
-  updateCountdown()
+  tempoRimanente = tempoTimer;
+  updateCountdown();
 
   contoAllaRovescia = setInterval(() => {
-    tempoRimanente--
-    updateCountdown()
+    tempoRimanente--;
+    updateCountdown();
 
     if (tempoRimanente <= 0) {
-      clearInterval(contoAllaRovescia)
-      tempoFinito()
+      clearInterval(contoAllaRovescia);
+      tempoFinito();
     }
-  }, 1000) //setInterval è un medtodo che richiama la funzione ogni tot secondi. va per millisecondi, 1000 = 1 secondo
-}
+  }, 1000); //setInterval è un medtodo che richiama la funzione ogni tot secondi. va per millisecondi, 1000 = 1 secondo
+};
 
 // window.addEventListener("load", inizioContoRovescia)
 // inizioContoRovescia() decommentare per vedere il timer partire
+
+const formSubmit = (e) => {
+  e.preventDefault();
+};
+
+const form = document.querySelector("form");
+form.addEventListener("submit", formSubmit);
