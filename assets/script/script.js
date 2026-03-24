203d16084ec42fc2a7fbe35ba91b58f4816ba7c8
Gianni <BussolettiGianni@gmail.com>
2026-03-24 14:21:03 +0100

    Update script.js

--- assets/script/script.js
+++ assets/script/script.js
@@ -284,12 +284,12 @@ const domande = {
 
 // SALVO LE DOMANDE PESCATE IN QUESTO ARRAY, IN MODO CHE NON POSSANO USCIRE NUOVAMENTE
 let domandePescate = []
-// DIFFICOLTà DOMANDE (BISOGNA AGGIUNGERE INPUT CHE FA SELEZIONARE DIFFICOLTà)
-let difficoltà = "facile"
 // PARTENZA DI BASE DEL PUNTEGGIO
 let punteggio = 0
 // CONTEGGIO DOMANDA CORRENTE
 let numeroDomandaCorrente = 1
+// DIFFICOLTà BASE
+let difficoltà = "facile"
 
 // FUNZIONE ESTRAZIONE DOMANDE
 
@@ -316,58 +316,75 @@ const estrazioneDomande = () => {
   domandePescate.push(numeroCasualeDomande)
 
   // ASSEGNAZIONE DOMANDA ALL'H2 E RISPOSTE AI BOTTONI
-  if (difficoltà === "facile") {
-    // QUI è DOVE VA IL TESTO DELLA DOMANDA
-    const h2 = document.getElementById("testo-domanda")
-    h2.innerText = `${domande.facile[numeroCasualeDomande].domanda}`
+  const domandaInBaseADifficoltà = domande[difficoltà][numeroCasualeDomande]
+  // QUI è DOVE VA IL TESTO DELLA DOMANDA
+  const h2 = document.querySelector("#testo-domanda h2")
+  h2.innerText = domandaInBaseADifficoltà.domanda
 
-    //   QUI ASSEGNO LE RISPOSTE AI BOTTONI
-    if (domande.facile[numeroCasualeDomande].tipo === "risposta multipla") {
-      btn1.innerText = `${domande.facile[numeroCasualeDomande].risposteSbagliate[1]}`
-      btn2.innerText = `${domande.facile[numeroCasualeDomande].risposteSbagliate[0]}`
-      btn3.classList.remove("no-display")
-      btn4.classList.remove("no-display")
-      btn3.innerText = `${domande.facile[numeroCasualeDomande].rispostaGiusta}`
-      btn4.innerText = `${domande.facile[numeroCasualeDomande].risposteSbagliate[2]}`
-    } else {
-      btn1.innerText = `${domande.facile[numeroCasualeDomande].rispostaGiusta}`
-      btn2.innerText = `${domande.facile[numeroCasualeDomande].risposteSbagliate}`
-      btn3.classList.add("no-display")
-      btn4.classList.add("no-display")
-    }
+  //   QUI ASSEGNO LE RISPOSTE AI BOTTONI
+  if (domandaInBaseADifficoltà.tipo === "risposta multipla") {
+    btn1.classList.remove("no-display")
+    btn2.classList.remove("no-display")
+    btn3.classList.remove("no-display")
+    btn4.classList.remove("no-display")
+    // MISCHIO LE RISPOSTE IN ORDINE CASUALE
+    const risposteMischiate = [
+      domandaInBaseADifficoltà.rispostaGiusta,
+      ...domandaInBaseADifficoltà.risposteSbagliate,
+    ].sort(() => Math.random() - 0.5)
 
-    //   CONTROLLO SE RISPOSTA è GIUSTA E IN CASO ASSEGNO I PUNTI
-    const risposta1 = document.getElementById("risposta-1")
-    risposta1.onclick = function () {
-      if (risposta1.innerText === domande.facile[numeroCasualeDomande].rispostaGiusta) {
-        punteggio += 1
-        console.log(punteggio)
-      }
-    }
+    btn1.innerText = risposteMischiate[0]
+    btn2.innerText = risposteMischiate[1]
+    btn3.innerText = risposteMischiate[2]
+    btn4.innerText = risposteMischiate[3]
+  } else {
+    btn1.classList.remove("no-display")
+    btn2.classList.remove("no-display")
+    btn1.innerText = domandaInBaseADifficoltà.rispostaGiusta
+    btn2.innerText = domandaInBaseADifficoltà.risposteSbagliate
+    btn3.classList.add("no-display")
+    btn4.classList.add("no-display")
+  }
 
-    const risposta2 = document.getElementById("risposta-2")
-    risposta2.onclick = function () {
-      if (risposta2.innerText === domande.facile[numeroCasualeDomande].rispostaGiusta) {
-        punteggio += 1
-        console.log(punteggio)
-      }
+  //   CONTROLLO SE RISPOSTA è GIUSTA E IN CASO ASSEGNO I PUNTI
+  const risposta1 = document.getElementById("risposta-1")
+  risposta1.onclick = function () {
+    if (risposta1.innerText === domandaInBaseADifficoltà.rispostaGiusta) {
+      punteggio += 1
     }
+    estrazioneDomande()
+    clearInterval(contoAllaRovescia)
+    inizioContoRovescia()
+  }
 
-    const risposta3 = document.getElementById("risposta-3")
-    risposta3.onclick = function () {
-      if (risposta3.innerText === domande.facile[numeroCasualeDomande].rispostaGiusta) {
-        punteggio += 1
-        console.log(punteggio)
-      }
+  const risposta2 = document.getElementById("risposta-2")
+  risposta2.onclick = function () {
+    if (risposta2.innerText === domandaInBaseADifficoltà.rispostaGiusta) {
+      punteggio += 1
     }
+    estrazioneDomande()
+    clearInterval(contoAllaRovescia)
+    inizioContoRovescia()
+  }
 
-    const risposta4 = document.getElementById("risposta-4")
-    risposta4.onclick = function () {
-      if (risposta4.innerText === domande.facile[numeroCasualeDomande].rispostaGiusta) {
-        punteggio += 1
-        console.log(punteggio)
-      }
+  const risposta3 = document.getElementById("risposta-3")
+  risposta3.onclick = function () {
+    if (risposta3.innerText === domandaInBaseADifficoltà.rispostaGiusta) {
+      punteggio += 1
     }
+    estrazioneDomande()
+    clearInterval(contoAllaRovescia)
+    inizioContoRovescia()
+  }
+
+  const risposta4 = document.getElementById("risposta-4")
+  risposta4.onclick = function () {
+    if (risposta4.innerText === domandaInBaseADifficoltà.rispostaGiusta) {
+      punteggio += 1
+    }
+    estrazioneDomande()
+    clearInterval(contoAllaRovescia)
+    inizioContoRovescia()
   }
 
   // AGGIORNAMENTO NUMERO DOMANDE BASE PAGINA
@@ -376,7 +393,7 @@ const estrazioneDomande = () => {
   questionsNum.innerText = `QUESTION ${numeroDomandaCorrente}/10`
   numeroDomandaCorrente += 1
 
-  checkNumeroDomande()
+  // inizioContoRovescia()
 }
 
 // ANIMAZIONE DEL TIMER
@@ -400,7 +417,9 @@ const updateCountdown = () => {
 }
 
 const tempoFinito = () => {
-  alert("tempo scaduto!")
+  estrazioneDomande()
+  inizioContoRovescia()
+
   // qui andranno inseriti i comportamenti di quello che succederà una volta che il timer sarà scaduto
 } // alert che avverte che il timer è scaduto
 
@@ -411,6 +430,7 @@ const inizioContoRovescia = () => {
   contoAllaRovescia = setInterval(() => {
     tempoRimanente--
     updateCountdown()
+    console.log(tempoRimanente)
 
     if (tempoRimanente <= 0) {
       clearInterval(contoAllaRovescia)
@@ -420,52 +440,25 @@ const inizioContoRovescia = () => {
 }
 
 // window.addEventListener("load", inizioContoRovescia)
-// inizioContoRovescia() decommentare per vedere il timer partire
+// inizioContoRovescia() //decommentare per vedere il timer partire
 
-const starsN = 10 // Numero di stelle desiderate
+// SELEZIONA DIFFICOLTà E INIZIA IL QUIZ
+const form = document.querySelector("#form-difficoltà")
+const select = document.querySelector("#scelta")
 
-// Creazione dinamica linea delle stelle
-const svgInjection = () => {
-  let divStar = document.getElementById("stelline")
+form.addEventListener("submit", function (e) {
+  e.preventDefault()
+  difficoltà = select.value
 
-  for (let i = 0; i < starsN; i++) {
-    divStar.innerHTML += divStar.innerHTML = `<span class="stelle" onclick="changeStyle(${i + 1})">
-        <svg
-          width="47"
-          height="46"
-          viewBox="0 0 47 46"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg">
-          <path
-            d="M22.2044 1.55551C22.6143 0.569963 24.0104 0.569964 24.4203 1.55552L29.9874 14.9402C30.1602 15.3557 30.5509 15.6396 30.9994 15.6756L45.4494 16.834C46.5134 16.9193 46.9448 18.2471 46.1341 18.9415L35.1248 28.3722C34.7831 28.6649 34.6338 29.1242 34.7382 29.5619L38.1018 43.6626C38.3494 44.7009 37.2199 45.5215 36.309 44.9651L23.9379 37.4089C23.5538 37.1743 23.0709 37.1743 22.6868 37.4089L10.3157 44.9651C9.40478 45.5215 8.27528 44.7009 8.52295 43.6626L11.8865 29.5619C11.9909 29.1242 11.8416 28.6649 11.4999 28.3722L0.490575 18.9415C-0.320069 18.2471 0.111362 16.9193 1.17535 16.834L15.6253 15.6756C16.0738 15.6396 16.4645 15.3557 16.6374 14.9402L22.2044 1.55551Z"
-            fill="#4c4e89" />
-        </svg>
-      </span>`
-  }
-}
-svgInjection()
-
-// Prima stella già con fill scuro
-const primaStella = document.querySelector("#stelline path")
-primaStella.setAttribute("fill", "#00FFFF")
-
-// cicli per cambiare dinamicamente il fill delle stelle
-const changeStyle = (n) => {
-  const img = document.querySelectorAll("#stelline path")
-
-  for (let i = 0; i < n; i++) {
-    img[i].setAttribute("fill", "#00FFFF")
-  }
-  for (let i = starsN - 1; i > n - 1; i--) {
-    img[i].setAttribute("fill", "#4c4e89")
-  }
-}
+  domandePescate = []
+  punteggio = 0
+  numeroDomandaCorrente = 1
+  estrazioneDomande()
+  inizioContoRovescia()
+})
 
 // Gestione form
 
-const formSubmit = (e) => {
-  e.preventDefault()
-}
-
-const form = document.querySelector("form")
-form.addEventListener("submit", formSubmit)
+// const formSubmit = (e) => {
+//   e.preventDefault()
+// }
