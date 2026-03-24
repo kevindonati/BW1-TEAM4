b184354990350bd3f355b98860ff4cefbc73cea4
Martina <acetomartina141@gmail.com>
2026-03-24 10:50:42 +0100

    grafico a torta
    Lo abbiamo inserito.

---
+++ assets/script/grafico.js
@@ -0,0 +1,23 @@
+const xValues = ["True", "False"];
+const yValues = [55, 44];
+const barColors = ["#b91d47", "#00aba9"];
+
+const ctx = document.getElementById("grafico-a-torta");
+
+new Chart(ctx, {
+  type: "doughnut",
+  data: {
+    labels: xValues,
+    datasets: [
+      {
+        backgroundColor: barColors,
+        data: yValues,
+      },
+    ],
+  },
+  options: {
+    plugins: {
+      legend: { display: false },
+    },
+  },
+});
